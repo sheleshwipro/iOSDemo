0832737bc6fe8006fc5c2ba17f866195936f41aa
sheleshwipro <shelesh.rawat@wipro.com>
2022-06-29 16:22:24 +0530

    Added tableview with static data.

--- iOSDemo/SceneDelegate.swift
+++ iOSDemo/SceneDelegate.swift
@@ -19,7 +19,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
+        let rootNav = UINavigationController(rootViewController: ViewController())
+        rootNav.isNavigationBarHidden = false
+        window.rootViewController = rootNav
         window.makeKeyAndVisible()
         self.window = window
         
--- iOSDemo/ViewController.swift
+++ iOSDemo/ViewController.swift
@@ -8,13 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var tableview:UITableView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        
+        configureUI()
+    }
+    
+    
+    //MARK: - configure UI
+    func configureUI(){
+        let tblView = UITableView()
+        tblView.delegate = self
+        tblView.dataSource = self
+        tblView.rowHeight = UITableView.automaticDimension
+        tblView.estimatedRowHeight = 1000
+        
+        view.addSubview(tblView)
+        tblView.translatesAutoresizingMaskIntoConstraints = false
+        tblView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        tblView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        tblView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tblView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
     }
 
 
 }
-
+extension ViewController:UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableview?.dequeueReusableCell(withIdentifier: "CellIdentifier")
+        {
+            cell.textLabel?.text = "\(indexPath.row)"
+            cell.detailTextLabel?.text = "test test test"
+            cell.imageView?.image = UIImage(named: "flag_of_canada")
+            return cell
+        }else {
+            let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "CellIdentifier")
+            cell.textLabel?.text = "\(indexPath.row)"
+            cell.detailTextLabel?.text = "test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test."
+            cell.detailTextLabel?.numberOfLines = 0
+            cell.imageView?.image = UIImage(named: "flag_of_canada")
+            return cell
+        }
+    }
+    
+    
+}
