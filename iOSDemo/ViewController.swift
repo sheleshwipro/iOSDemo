e348549276e52c560af667b0b5562cf460ed92e9
sheleshwipro <shelesh.rawat@wipro.com>
2022-06-29 12:53:53 +0530

    first commit

---
+++ iOSDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSDemo
+//
+//  Created by Apple on 29/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
